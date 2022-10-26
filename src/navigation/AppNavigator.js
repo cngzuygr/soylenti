81626c1a25e041e643ad1b99e06da6f49162ab70
cngzuygr <59419417+cngzuygr@users.noreply.github.com>
2022-10-26 13:30:19 +0300

    [FIX] LocationScreen radiusbox

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,14 +1,61 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { HomeScreen, LocationScreen } from "../screens";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
 const AppNav = createNativeStackNavigator();
 
-export default function AppNavigator() {
-	return (
-		<AppNav.Navigator screenOptions={{ headerShown: false }}>
-			<AppNav.Screen name="HomeScreen" component={HomeScreen} />
-			<AppNav.Screen name="LocationScreen" component={LocationScreen} />
-		</AppNav.Navigator>
-	);
+export default function AppNavigator({ navigation }) {
+  return (
+    <AppNav.Navigator>
+      <AppNav.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          headerTitle: "Söylenti",
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            color: "#7965C3",
+            fontSize: 30,
+            fontFamily: "GloriaRegular",
+            textShadowOffset: { width: 1, height: 1 },
+            textShadowColor: "black",
+            textShadowRadius: 1,
+          },
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{
+                marginLeft: 10,
+                marginTop: 10,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={() => navigation.navigate("LocationScreen")}
+            >
+              <Ionicons name="md-location" size={32} color="#7965C3" />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              style={{
+                marginRight: 10,
+                marginTop: 10,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={() => navigation.navigate("SettingsScreen")}
+            >
+              <MaterialIcons name="settings" size={32} color="#7965C3" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <AppNav.Screen
+        name="LocationScreen"
+        component={LocationScreen}
+        options={{ headerShown: false }}
+      />
+    </AppNav.Navigator>
+  );
 }
--- src/screens/LocationScreen.js
+++ src/screens/LocationScreen.js
@@ -1,177 +1,185 @@
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import React, { useState } from "react";
-import MapView, { Animated, Circle } from "react-native-maps";
+import { Animated, Circle } from "react-native-maps";
 import { AntDesign } from "@expo/vector-icons";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { Slider } from "@miblanchard/react-native-slider";
+import fontTypes from "../theme/fonts";
 
 export default function LocationScreen({ navigation }) {
-	const [radiusSelected, setRadiusSelected] = useState({ radius: 0 });
-	const [customSelected, setCustomSelected] = useState(false);
-	const location = {
-		coords: {
-			longitude: 32.52243,
-			latitude: 38.01113,
-		},
-	};
+  const [radiusSelected, setRadiusSelected] = useState({ radius: 1000 });
+  const [customSelected, setCustomSelected] = useState(true);
+  const location = {
+    coords: {
+      longitude: 32.52243,
+      latitude: 38.01113,
+    },
+  };
 
-	const radiusSelection = [
-		{
-			key: 1,
-			title: "1km",
-			radius: 500,
-		},
-		{
-			key: 2,
-			title: "5km",
-			radius: 2500,
-		},
-		{
-			key: 3,
-			title: "20km",
-			radius: 10000,
-		},
-		{
-			key: 4,
-			title: "50km",
-			radius: 25000,
-		},
-	];
-	return (
-		<View style={{ flex: 1 }}>
-			<Animated
-				style={{ flex: 1 }}
-				region={{
-					latitude: location.coords.latitude || 38.01113,
-					longitude: location.coords.longitude || 32.52243,
-					latitudeDelta: radiusSelected.radius / 40000,
-					longitudeDelta: radiusSelected.radius / 40000,
-				}}
-			>
-				<Circle
-					center={{
-						latitude: location.coords.latitude || 38.01113,
-						longitude: location.coords.longitude || 32.52243,
-					}}
-					radius={radiusSelected.radius}
-					strokeWidth={3}
-					strokeColor="#0000FF44"
-					fillColor="#FF000077"
-				/>
-			</Animated>
-			<View
-				style={{
-					height: "100%",
-					width: "100%",
-					position: "absolute",
-					justifyContent: "space-between",
-				}}
-			>
-				<TouchableOpacity
-					style={{
-						width: 50,
-						height: 50,
-						marginTop: 40,
-						marginLeft: 30,
-						backgroundColor: "#FF000077",
-						justifyContent: "center",
-						alignItems: "center",
-						borderRadius: 20,
-					}}
-					onPress={() => navigation.goBack()}
-				>
-					<AntDesign name="left" size={24} color="white" />
-				</TouchableOpacity>
-				<View
-					style={{
-						flexDirection: "column",
-						borderRadius: 50,
-						width: "80%",
-						backgroundColor: "#FF000077",
-						marginBottom: 50,
-						alignSelf: "center",
-					}}
-				>
-					<Slider
-						value={(radiusSelected.radius * 2) / 1000}
-						onValueChange={(value) => {
-							setRadiusSelected({ radius: (value / 2) * 1000 });
-						}}
-						minimumValue={0.1}
-						maximumValue={50}
-						step={0.1}
-						containerStyle={{ width: "80%", alignSelf: "center" }}
-						minimumTrackTintColor="#8883f0"
-						maximumTrackTintColor="#00000066"
-						thumbTintColor="#8883f0"
-					/>
-					<View
-						style={{
-							flexDirection: "row",
-							alignSelf: "center",
-							marginBottom: 20,
-						}}
-					>
-						<View style={styles.buttonContainer}>
-							{radiusSelection.map(({ key, title, radius }) => (
-								<TouchableOpacity
-									onPress={() => {
-										setRadiusSelected({
-											key: key,
-											title: title,
-											radius: radius,
-										});
-										setCustomSelected(false);
-									}}
-									key={key}
-									style={{
-										width: "20%",
-										alignItems: "center",
-										backgroundColor: `${
-											radiusSelected?.key == key ? "#8883f0" : "white"
-										}`,
-										height: 35,
-										justifyContent: "center",
-										marginHorizontal: 5,
-										borderRadius: 5,
-									}}
-								>
-									<Text style={styles.buttonText}>{title}</Text>
-								</TouchableOpacity>
-							))}
-							<TouchableOpacity
-								style={{
-									height: 36,
-									width: 36,
-									borderRadius: 5,
-									backgroundColor: "green",
-									padding: 10,
-									justifyContent: "center",
-									alignItems: "center",
-								}}
-							>
-								<AntDesign name="check" size={16} color="white" />
-							</TouchableOpacity>
-						</View>
-					</View>
-				</View>
-			</View>
-			<StatusBar />
-		</View>
-	);
+  const radiusSelection = [
+    {
+      key: 1,
+      title: "1km",
+      radius: 500,
+    },
+    {
+      key: 2,
+      title: "5km",
+      radius: 2500,
+    },
+    {
+      key: 3,
+      title: "20km",
+      radius: 10000,
+    },
+  ];
+  return (
+    <View style={{ flex: 1 }}>
+      <Animated
+        style={{ flex: 1 }}
+        region={{
+          latitude: location.coords.latitude || 38.01113,
+          longitude: location.coords.longitude || 32.52243,
+          latitudeDelta: radiusSelected.radius / 30000,
+          longitudeDelta: radiusSelected.radius / 30000,
+        }}
+      >
+        <Circle
+          center={{
+            latitude: location.coords.latitude || 38.01113,
+            longitude: location.coords.longitude || 32.52243,
+          }}
+          radius={radiusSelected.radius}
+          strokeWidth={3}
+          strokeColor="#0000FF44"
+          fillColor="#FF000077"
+        />
+      </Animated>
+      <View style={styles.container}>
+        <View style={styles.backButtonContainer}>
+          <TouchableOpacity
+            style={styles.backButton}
+            onPress={() => navigation.goBack()}
+          >
+            <AntDesign name="left" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.radiusContainer}>
+          <Slider
+            value={(radiusSelected.radius * 2) / 1000}
+            onValueChange={(value) => {
+              setRadiusSelected({ radius: (value / 2) * 1000 });
+            }}
+            minimumValue={0.1}
+            maximumValue={50}
+            step={0.1}
+            containerStyle={{ width: "80%", alignSelf: "center" }}
+            minimumTrackTintColor="#8883f0"
+            maximumTrackTintColor="#00000066"
+            thumbTintColor="#8883f0"
+            onTouchEnd={() => {
+              setCustomSelected(true);
+            }}
+          />
+          <View style={styles.buttonContainer}>
+            {radiusSelection.map(({ key, title, radius }) => (
+              <TouchableOpacity
+                onPress={() => {
+                  setRadiusSelected({
+                    key: key,
+                    title: title,
+                    radius: radius,
+                  });
+                  setCustomSelected(false);
+                }}
+                key={key}
+                style={[
+                  styles.radiusButton,
+                  {
+                    backgroundColor: `${
+                      radiusSelected?.key == key ? "#8883f0" : "white"
+                    }`,
+                  },
+                ]}
+              >
+                <Text style={styles.radiusButtonText}>{title}</Text>
+              </TouchableOpacity>
+            ))}
+            <TouchableOpacity
+              style={[
+                styles.radiusButton,
+                {
+                  backgroundColor: `${customSelected ? "#8883f0" : "white"}`,
+                },
+              ]}
+            >
+              <Text style={styles.radiusButtonText}>
+                {((radiusSelected.radius * 2) / 1000).toFixed(1.1)}km
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.saveButton}>
+              <AntDesign name="check" size={16} color="white" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <StatusBar />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	buttonContainer: {
-		flexDirection: "row",
-		alignItems: "center",
-		justifyContent: "center",
-		marginTop: 15,
-		width: "80%",
-		alignSelf: "center",
-	},
-	buttonText: {
-		fontSize: 14,
-	},
+  container: {
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    justifyContent: "space-between",
+  },
+  backButtonContainer: { width: "90%", alignSelf: "center", marginTop: 40 },
+  backButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#FF000077",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  radiusContainer: {
+    flexDirection: "column",
+    borderRadius: 20,
+    width: "90%",
+    backgroundColor: "#FF000077",
+    marginBottom: 30,
+    alignSelf: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 15,
+    width: "90%",
+    alignSelf: "center",
+  },
+  radiusButton: {
+    height: 35,
+    justifyContent: "center",
+    marginHorizontal: 5,
+    borderRadius: 5,
+    width: "20%",
+    alignItems: "center",
+  },
+  radiusButtonText: {
+    fontSize: fontTypes.size.font14,
+    fontFamily: fontTypes.type.montserratRegular,
+  },
+  saveButton: {
+    height: 36,
+    width: 36,
+    borderRadius: 5,
+    backgroundColor: "green",
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
